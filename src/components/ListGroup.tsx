7ea1354ef3f9f89e2ee27b375dd5960376dfcdfc
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-06-21 04:50:04 +0500

    adding bootstrap and use list group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">A item</li>
+      <li className="list-group-item">A second Item</li>
+      <li className="list-group-item">A third Item</li>
+      <li className="list-group-item">A forth Item</li>
+      <li className="list-group-item">A fifth Itrem</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
