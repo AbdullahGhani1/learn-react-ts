8aa23dfe2d21b41a3de2b647788b4292e80a7423
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-07-30 16:47:01 +0500

    form submittion and useRef

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,22 @@
+import { FormEvent, useRef } from 'react';
 import FormItem from './FormItem';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: '', age: 0 };
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    person.name = nameRef.current?.value || '';
+    person.age = Number(ageRef.current?.value);
+
+    console.log(person);
+  };
   return (
-    <form id="form" className="bg-dark text-white p-3">
-      <FormItem label="Name" type="text" />
-      <FormItem label="Age" type="number" />
+    <form id="form" className="bg-dark text-white p-3" onSubmit={handleSubmit}>
+      <FormItem label="Name" type="text" ref={nameRef} />
+      <FormItem label="Age" type="number" ref={ageRef} />
+      <input className="btn btn-primary" type="submit" title="Submit" />
     </form>
   );
 };
--- src/components/Form/FormItem.tsx
+++ src/components/Form/FormItem.tsx
@@ -1,16 +1,25 @@
+import React, { ForwardedRef } from 'react';
+
 interface Props {
   label?: string;
   type?: string;
 }
-const FormItem = (props: Props) => {
-  const { label, type = 'text' } = props;
-  return (
-    <div className="mb-3">
-      <label htmlFor={type.toLowerCase()} className="form-label">
-        {label}
-      </label>
-      <input type={type} id={type.toLowerCase()} className="form-control" />
-    </div>
-  );
-};
+const FormItem = React.forwardRef<HTMLInputElement, Props>(
+  (props: Props, ref: ForwardedRef<HTMLInputElement>) => {
+    const { label, type = 'text' } = props;
+    return (
+      <div className="mb-3">
+        <label htmlFor={type.toLowerCase()} className="form-label">
+          {label}
+        </label>
+        <input
+          ref={ref}
+          type={type}
+          id={type.toLowerCase()}
+          className="form-control"
+        />
+      </div>
+    );
+  }
+);
 export default FormItem;
